7e13ce1b7ea574277e570705ae5df7a14c0ab525
Daniel Hoesle <d1@PacBook-Mro.local>
2020-06-09 23:06:39 -0600

    completed search bar and posts

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,34 +1,39 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
+
+// import dummyData from '../../dummy-data'
 
 import "./Posts.css";
 
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
+  const { postData } = props
 
+  const [likes, setlikes] = useState(props)
+  console.log(likes.postData.username)
   return (
     <div className="post-border">
       <PostHeader
-        username={props.post.username}
+        username={likes.postData.username}
         thumbnailUrl={
-          props.post.thumbnailUrl
+          likes.postData.thumbnailUrl
         }
       />
       <div className="post-image-wrapper">
         <img
           alt="post thumbnail"
           className="post-image"
-          src={props.post.imageUrl}
+          src={likes.postData.imageUrl}
         />
       </div>
       <LikeSection />
       <CommentSection
-        postId={props.post.imageUrl}
-        comments={props.post.comments}
+        postId={likes.postData.imageUrl}
+        comments={likes.postData.comments}
       />
     </div>
   );
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,31 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from '../../dummy-data'
 
 const PostsPage = () => {
   // set up state for your data
+const [posts, setPosts] = useState(dummyData)
+// console.log("PostsPage -> posts", posts)
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {posts.map(postObject => {
+        console.log(postObject)
+        return (
+          <Post key={postObject.username} postData={postObject}/>
+        )
+        
+        
+      })
+      }
+
+
+
     </div>
   );
 };
